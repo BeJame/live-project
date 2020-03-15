eb847f61b4d4a6ff187c77d648728843d0f5e587
ldc-37 <ldc791551236@gmail.com>
2020-03-15 11:42:35 +0800

    request init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,12 +11,17 @@ export default {
 </script>
 
 <style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+html, body {
+  font-family: 'Microsoft YaHei';
+  /* height: 100vh; */
+}
+
+.el-container, header.el-header, .el-main {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  height: 100%;
+}
 </style>
---
+++ frontend/src/network/axios.js
@@ -0,0 +1,67 @@
+import { Message, MessageBox } from 'element-ui'
+
+import axios from 'axios'
+import router from '../router'
+import store from '../store'
+
+const instance = axios.create({
+  baseURL: process.env.NODE_ENV === "development" ? '/api' : '/api', 
+  headers: {
+    // Cookie: 'SESSION=Y2EyNWQ5NDUtMzk2Yy00OTQxLTgyMjktZWVhMTFjNGYwNGMx' // not allowed
+  }
+})
+
+instance.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+  )
+  
+  instance.interceptors.response.use(
+    res => {
+    // if (typeof(res.data) === 'string' && res.data.includes('<!DOCTYPE html>')) {
+    //   MessageBox.alert('登录过期或尚未登录，请重新登录。', '提醒').then(_ => {
+    //     store.commit('setLoginState', false)
+    //     router.push('/login')
+    //   })
+    //   return
+    // } else if (typeof(res.data) === 'string') {
+    //   // useless
+    //   res.headers['content-type'] = 'application/pdf'
+    //   console.log(res.headers)
+    //   return res.data // special for download
+    // }
+
+    // if (res.data.code === 0) {
+    //   return res.data.data
+    // } else {
+    //   console.warn(res)
+    //   Message.error({
+    //     message: '服务器错误，错误码：' + res.data.code,
+    //     duration: '4000'
+    //   })
+      return res.data
+    // }
+  },
+  async error => {
+    console.warn(Object.entries(error))
+    if (error.response.status.toString()[0] === '4') {
+      Message.error({
+        message: `${error.response.data.data.msg}(${error.response.data.code})`,
+        duration: '3000'
+      })
+    } else {
+      Message.error({
+        message: '请求 ' + error.config.url + ' 异常，状态码：' + error.response.status,
+        duration: '5000'
+      })
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default instance
