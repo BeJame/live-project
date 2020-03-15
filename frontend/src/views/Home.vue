049b9e3fc6705663be07d6fb3db5c63afdf02f45
ldc-37 <ldc791551236@gmail.com>
2020-03-15 11:33:34 +0800

    init frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    init
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: '',
+  components: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  
+</style>
